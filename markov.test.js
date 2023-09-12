f051ad13c745f0a7d82a7a225693b542fe2d7488
ross-brown <rbrown_98@yahoo.com>
2023-09-12 18:36:50 -0500

    write more tests

--- markov.test.js
+++ markov.test.js
@@ -10,7 +10,7 @@ describe("getChains function", function () {
   test("returns markovChain Obj", function () {
     const markovChain = machine.getChains();
     expect(markovChain).toEqual({
-      "the": ["cat","hat"],
+      "the": ["cat", "hat"],
       "cat": ["in"],
       "in": ["the"],
       "hat": [null],
@@ -23,17 +23,29 @@ describe("getText function", function () {
 
   let machine;
   beforeEach(function () {
-    machine = new MarkovMachine("the cat in hat is fat");
+
   });
 
-  test("returns markovChain Obj", function () {
+  test("returns random markov text", function () {
+    machine = new MarkovMachine("the cat in hat is fat");
+
     const text = machine.getText();
     expect(text).toEqual("the cat in hat is fat");
 
   });
+
+  test("returns random markov text", function () {
+    machine = new MarkovMachine("The cat is in the hat. The cat is the cat. The hat is a cat.");
+
+    const text = machine.getText();
+    expect(text).toMatch(/^The/);
+    expect(text).toMatch(/cat\.$/);
+  });
+
+
 });
 
-test("returns random idx",function(){
+test("returns random idx", function () {
   const randomIdx = MarkovMachine.generateRandomIdx(3);
-  expect([0,1,2]).toContain(randomIdx);
-})
+  expect([0, 1, 2]).toContain(randomIdx);
+});
